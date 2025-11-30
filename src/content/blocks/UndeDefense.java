838300717f1203695df9ed5e9fe1a87af93fecd7
Aerodynamic-Lawyer <edwpipe11@gmail.com>
2025-11-29 20:53:50 -0500

    Create UndeDefense.java

---
+++ src/content/blocks/UndeDefense.java
@@ -0,0 +1,14 @@
+package content.blocks
+
+import mindustry.content.Fx;
+import mindustry.content.Planets;
+import mindustry.entities.bullet.BasicBulletType;
+import mindustry.entities.bullet.EmptyBulletType;
+import mindustry.entities.effect.ExplosionEffect;
+import mindustry.graphics.Pal;
+import mindustry.type.Category;
+import mindustry.world.Block;
+import mindustry.world.meta.Env;
+
+public class UndeDefense {
+    public static Block leadwall, largeleadwall;
